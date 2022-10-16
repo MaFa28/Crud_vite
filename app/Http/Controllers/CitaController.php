7efd0044d7505189867dc1cbb02280d039a9e42f
Marco Morales <marcomorales.mm41@gmail.com>
2022-10-15 22:29:58 -0500

    Agregando modelo,controller,migracion

---
+++ app/Http/Controllers/CitaController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cita;
+use Illuminate\Http\Request;
+
+class CitaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $citas = Cita::all();
+        return view('inicio', compact('citas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('citas');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'nombre' => 'required|max:255',
+            'correo' => 'required',
+            'telefono' => 'required|max:20',
+            'tipomascota' => 'required|max:20|min:3',
+            'raza' => 'required|max:20|min:3',
+            'comentario' => 'required',
+        ]);
+
+        Cita::create($request->all());
+
+        return redirect('/citas');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cita  $cita
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cita $cita)
+    {
+        //
+        return view('citasShow',compact('cita'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cita  $cita
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cita $cita)
+    {
+        //
+        return view('citasEdit',compact('cita'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cita  $cita
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cita $cita)
+    {
+        //
+        $request->validate([
+            'nombre' => 'required|max:255',
+            'correo' => 'required',
+            'telefono' => 'required|max:20',
+            'tipomascota' => 'required|max:20|min:3',
+            'raza' => 'required|max:20|min:3',
+            'comentario' => 'required',
+        ]);
+
+        Cita::where('id', $cita->id)->update($request->except('_token','_method'));
+
+        return redirect('/citas');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cita  $cita
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cita $cita)
+    {
+        //
+        $cita->delete();
+        return redirect('/citas');
+    }
+}
---
+++ database/migrations/2022_10_16_032324_create_citas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('correo');
+            $table->string('telefono');
+            $table->string('tipomascota');
+            $table->string('raza');
+            $table->text('comentario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citas');
+    }
+};
